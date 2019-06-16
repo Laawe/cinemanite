a58d05b747221030f03aee4e6181e3479409d17c
Laawe <laura.render.03@gmail.com>
2019-06-15 22:30:08 -0500

    Updates

---
+++ precache-manifest.2a91bcefdcdc3c2c759e9f98048ff1d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308cc95ef00bd7ba8ee29163df0b8529",
+    "url": "/cinemanite/index.html"
+  },
+  {
+    "revision": "4e2ea4e5d2fd72148d92",
+    "url": "/cinemanite/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "23e4a88c500969359f92",
+    "url": "/cinemanite/static/css/main.49e71d0b.chunk.css"
+  },
+  {
+    "revision": "4e2ea4e5d2fd72148d92",
+    "url": "/cinemanite/static/js/2.177fd765.chunk.js"
+  },
+  {
+    "revision": "23e4a88c500969359f92",
+    "url": "/cinemanite/static/js/main.7123d6ed.chunk.js"
+  },
+  {
+    "revision": "a98bb818933928bae81d",
+    "url": "/cinemanite/static/js/runtime~main.2eb06c1a.js"
+  }
+]);
