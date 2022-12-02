b9aa13a8b4cd5d6d05804b09767ec46bc41d6a3f
Laawe <laura.render.03@gmail.com>
2022-12-02 00:00:01 -0600

    feat: add homepage header section volumen I

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
+import Image from "next/image";
 import { Layout } from "../components/layout";
+import cinemanite from '../public/cinemanite.gif';
 import styles from '../styles/homepage.module.css';
 
 const HomePage = () => {
     return (
         <Layout>
             <section className={styles['homepage-header']}>
-                <h1
-                    className={styles['homepage-title']}>Acción!</h1>
-                    <p>Hagamos cine</p>
+                <div className={styles['homepage-wrap-gif']}>
+                    <Image
+                        className={styles['homepage-gif']}
+                        src={cinemanite}
+                        alt="cinemanite logo gif" />
+                </div>
+                <div className={styles['homepage-header-text']}>
+                    <h1 className={styles['homepage-title']}>¡Acción!</h1>
+                    <div className={styles['homepage-wrap-subtitle']}>
+                        {/* <span className={styles['homepage-emoji']} role="img" aria-label="film camera">🎥</span> */}
+                        <span className={styles['homepage-subtitle']}>
+                            Cine, audiovisual. Colaboramos en el diseño y creación de tu proyecto. Bienvenido a <span className={styles['homepage-sub-subtitle']}><strong>Cinemanite</strong> /cinEma,nAit/ </span>
+                        </span>
+                        {/* <section className={styles['homepage-subtitle-animation-wrap']}>
+                            <div className={styles['homepage-subtitle-animation-first']}><p>cine</p></div>
+                            <div className={styles['homepage-subtitle-animation-second']}><p>audiovisual</p></div>
+                            <div className={styles['homepage-subtitle-animation-third']}><p>cinemanite</p></div>
+                        </section> */}
+                    </div>
+                    <button className={styles['homepage-header-btn']}>Conoce más</button>
+                </div>
             </section>
         </Layout>
     )
