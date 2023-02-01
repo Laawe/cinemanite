61f4a65cad32fc25c89a9bff9237e93f21bb5fb6
Laawe <laura.render.03@gmail.com>
2023-01-31 19:44:44 -0600

    feat: add basic navbar

--- components/forehead/forehead.js
+++ components/forehead/forehead.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { CustomLink } from '../microcomponents/custom-link/custom-link';
+import Link from 'next/link';
+import Image from 'next/image';
+import cinemanite from '../../public/cinemanite.jpg';
+import styles from './forehead.module.css'
 
 export const Forehead = () => {
   return (
-    <section>
-      <CustomLink label="cinemanite" href="/" />
+    <section className={styles['forehead-wrapper']}>
+      <Link href="/" className={styles['forehead-home-link']}>
+        <Image className={styles['forehead-home-link-image']} src={cinemanite} alt="home icon cinemanite" />
+      </Link>
+      <Link className={styles['forehead-manifiesto-link']} href="/manifiesto"><em className={styles['forehead-manifiesto-link--em']}>manifiesto</em></Link>
     </section>
   )
 }
