d07bf99091211a2d4393eb9e721d8fc6684ce89b
Laawe <laura.render.03@gmail.com>
2023-02-09 22:56:45 -0600

    feat: adjust card component to desired look and feel

--- components/microcomponents/card.js
+++ components/microcomponents/card.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import styles from './card.module.css';
-import service1 from '../../public/service1.jpg';
 
 export const Card = () => {
   return (
     <section className={styles['card-wrapper']}>
-        <Image src={service1} alt='offer service image' className={styles['card-image']}></Image>
-        <div className={styles['card-label-wrapper']}>
-          <h5>Video memoria de eventos</h5>
-          <p>Apoyamos en la expansión de tu idea a través de un proyecto audiovisual</p>
-          <button>
-              <Link href={'https://www.youtube.com/@cinemanite/videos'}>Mira nuestro trabajo previo.</Link>
-          </button>
+      <div className={styles['card-card']}>
+        <div className={styles['card-content']}>
+          <h2 className={styles['card-title']}>Video memoria de eventos</h2>
+          <p className={styles['card-text']}>Trabajamos en la expansión de tu idea a través de un proyecto/memoria audiovisual</p>
+          <Link href={'https://www.youtube.com/@cinemanite/videos'}>Mira nuestro trabajo previo.</Link>
         </div>
+      </div>
     </section>
   )
 }
